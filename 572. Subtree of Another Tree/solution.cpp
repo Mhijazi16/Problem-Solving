f2ac693243e495b15b2d98cc38af6fe76acfea36
mhijazi16 <gultimate0@gmail.com>
2025-04-16 12:09:23 +0300

    solved again

--- 572. Subtree of Another Tree/solution.cpp
+++ 572. Subtree of Another Tree/solution.cpp
@@ -1,45 +1,39 @@
 
-struct TreeNode{
-  int val; 
-  TreeNode* left; 
-  TreeNode* right; 
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
 
-  TreeNode(): val(0), left(nullptr), right(nullptr) {}
-  TreeNode(int val): val(val), left(nullptr), right(nullptr) {}
-  TreeNode(int val, TreeNode* left, TreeNode* right): val(val), left(left), right(right) {}
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int val) : val(val), left(nullptr), right(nullptr) {}
+  TreeNode(int val, TreeNode *left, TreeNode *right)
+      : val(val), left(left), right(right) {}
 };
 
+class Solution {
+public:
+  bool isSameTree(TreeNode *one, TreeNode *two) {
+    if (!one && !two)
+      return true;
 
-class Solution{
-  public: 
-    bool isSameTree(TreeNode* one, TreeNode* two){
-
-      if (!one && !two){
-        return true;
-      }
-      
-      if (one && !two || two && !one || one->val != two->val) {
-        return false;
-      }
-
-      return isSameTree(one->left, two->left) && isSameTree(one->right, two->right);
+    if (!one || !two || one->val != two->val) {
+      return false;
     }
 
-    bool isSubtree(TreeNode* root, TreeNode* subtree){
+    return isSameTree(one->left, two->left) &&
+           isSameTree(one->right, two->right);
+  }
 
-      if (!root) {
-        return subtree == nullptr;
-      }
-       
-      if (root->val == subtree->val){
-       if (isSameTree(root, subtree) == false){
-        return isSubtree(root->left, subtree) || isSubtree(root->right, subtree);
-       }
-
-       return true;
-      } 
-
-      return isSubtree(root->left, subtree) || isSubtree(root->right, subtree);
+  bool isSubtree(TreeNode *root, TreeNode *subtree) {
+    if (root == nullptr) {
+      return false;
     }
+
+    if (root->val == subtree->val && isSameTree(root->left, subtree->left) &&
+        isSameTree(root->right, subtree->right)) {
+      return true;
+    }
+
+    return isSubtree(root->left, subtree) || isSubtree(root->right, subtree);
+  }
 };
-
