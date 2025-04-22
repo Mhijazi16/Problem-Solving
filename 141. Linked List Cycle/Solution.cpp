2a13d0b10045d1184f097d5cfba8f874ff931a5c
mhijazi16 <gultimate0@gmail.com>
2025-04-22 22:12:31 +0300

    solved

---
+++ 141. Linked List Cycle/Solution.cpp
@@ -0,0 +1,27 @@
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class Solution {
+public:
+  bool hasCycle(ListNode *head) {
+
+    if (head == nullptr)
+      return false;
+
+    auto slow = head;
+    auto fast = head->next;
+
+    while (fast != nullptr && fast->next != nullptr) {
+      if (fast == slow) {
+        return true;
+      }
+      slow = slow->next;
+      fast = fast->next->next;
+    }
+    return false;
+  }
+};
