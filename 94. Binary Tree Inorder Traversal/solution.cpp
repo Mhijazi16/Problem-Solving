c8b550dec1835ea9dbd9a3a06ad36eb66a5a5d5c
mhijazi16 <gultimate0@gmail.com>
2025-08-02 11:37:00 +0300

    solved

---
+++ 94. Binary Tree Inorder Traversal/solution.cpp
@@ -0,0 +1,35 @@
+#include <functional>
+#include <vector>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+      vector<int> ans; 
+
+      function<void(TreeNode*)> DFS; 
+      DFS = [&](TreeNode* node){ 
+        if (node == nullptr) {
+          return;
+        }
+
+        DFS(node->left);
+        ans.push_back(node->val);
+        DFS(node->right);
+      };
+
+
+      DFS(root);
+      return ans;
+    }
+};
